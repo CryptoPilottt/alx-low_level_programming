5e55616364ffc96dd24863b27872953e0b543491
Sheila <nyamarisheila@gmail.com>
2023-03-21 12:48:57 -0700

    lowercase alphabet followed by new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - print alphabet in lowercase
+ *
+ * Return: 0 if successful
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
