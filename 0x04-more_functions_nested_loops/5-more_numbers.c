98a67531ccea4f707ef0c338c81aa39c3d97c778
Sheila <nyamarisheila@gmail.com>
2023-03-24 16:32:55 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 5-main.c 5-more_numbers.c -o 5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,11 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 1; x < 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y > 9)
-			{
-				putchar((y / 10) + '0');
-			}
-			putchar((y % 10) + '0');
+			putchar(y + '0');
 		}
 		putchar('\n');
 	}
