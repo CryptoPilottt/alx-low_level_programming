1e079f206775a602a0d62886988ea34b1709bb23
Sheila <nyamarisheila@gmail.com>
2023-04-11 09:06:28 -0700

    no risk no reward 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *str_concat - concatenates two strings
+ *@s1: input 1
+ *@s2: input 2
+ *Return: 0 if successful
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *conct;
+	int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	i = j = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	conct = malloc(sizeof(char) * (i + j + 1));
+	if (conct == NULL)
+		return (NULL);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
+	conct[i] = '\0';
+	return (conct);
+}
