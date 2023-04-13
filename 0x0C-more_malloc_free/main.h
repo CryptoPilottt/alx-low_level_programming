f3791f397225775705b89f8b781574a7e1b138f3
Sheila <nyamarisheila@gmail.com>
2023-04-13 16:34:02 -0700

    ./101-mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memset(char *s, char c, unsigned int n);
+int main(int argc, char *argv[]);
 
 #endif
